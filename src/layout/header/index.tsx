fd3de9613eccd523c5350c5c5515b0e220587f66
JD_Rodrigues <jdevproject01@gmail.com>
2023-01-24 00:07:34 -0300

    Feat - header

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,8 +1,41 @@
+import { Cart, User } from "@styled-icons/boxicons-regular"
+import Image from "next/image"
+import logo from 'public/logo.png'
+import { useEffect, useState } from "react"
+
 
 
 const Header = () => {
+  const [screenWidth, setScreenWidth] = useState(Number)
+
+  const setWindowWidth = () => {
+    setScreenWidth(window.innerWidth)
+  }
+
+  useEffect(()=>{
+    window.addEventListener('resize', setWindowWidth)
+  },[])
+ 
   return (
-    <div className="header">Header</div>
+    <div className='header container'>
+      <Image 
+        className="logo"
+        src={logo}
+        alt='Logotipo Kampler Store'
+      />
+      <nav>
+        <ul className='header__menu__list' >
+          <li>Accounts</li>
+          <li>Missions</li>
+          <li>Power leveling</li>
+          <li>Weapons</li>
+        </ul>
+      </nav>
+      <div className="user__panel">
+      {screenWidth >= 768 && <User className="user__icon" />}
+      <Cart className="cart__icon" />
+      </div>
+    </div>
   )
 }
 
