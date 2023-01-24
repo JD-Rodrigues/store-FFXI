45251f217252e8f27f55960c659165ab4a050600
JD_Rodrigues <jdevproject01@gmail.com>
2023-01-24 17:39:36 -0300

    Style - Responsividade do header

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,5 +1,6 @@
 import { Cart, User } from "@styled-icons/boxicons-regular"
 import Image from "next/image"
+import Link from "next/link"
 import logo from 'public/logo.png'
 import { useEffect, useState } from "react"
 
@@ -14,26 +15,54 @@ const Header = () => {
 
   useEffect(()=>{
     window.addEventListener('resize', setWindowWidth)
+    setWindowWidth()
   },[])
  
   return (
     <div className='header container'>
-      <Image 
+      <p className="mobile__menu__btn">Menu</p>
+      <Link
+        href="/"
+      >
+        <Image 
         className="logo"
         src={logo}
         alt='Logotipo Kampler Store'
       />
-      <nav>
+      </Link>
+      
+      <nav className="header__menu">
         <ul className='header__menu__list' >
-          <li>Accounts</li>
-          <li>Missions</li>
-          <li>Power leveling</li>
-          <li>Weapons</li>
+          <Link 
+            href="" 
+            className="header__menu__item link"
+          >
+            Accounts
+          </Link>
+          <Link
+            href="" 
+            className="header__menu__item link"
+          >
+            Missions
+          </Link>
+          <Link
+            href="" 
+            className="header__menu__item link"
+          >
+            Power leveling
+          </Link>
+          <Link
+            href="" 
+            className="header__menu__item link"
+          >
+            Weapons
+          </Link>
         </ul>
       </nav>
       <div className="user__panel">
-      {screenWidth >= 768 && <User className="user__icon" />}
-      <Cart className="cart__icon" />
+      {screenWidth >= 768 && 
+        <User className="user__icon" />}
+        <Cart className="cart__icon" />
       </div>
     </div>
   )
